a8b7b05b3d68524c9de72332c3a10ebe8d7b41b7
Masashi <ume.app555@gmail.com>
2020-04-18 16:51:05 +0900

    make react app template

---
+++ README.md
@@ -0,0 +1,6 @@
+# react16_babel7_webpack4
+
+template of react app
+
+Please see below page
+https://kara100.com/javascript/react16-babel7-webpack4/
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const isDebug = process.env.NODE_ENV !== 'production';
+console.log(`debug is ${isDebug}`);
+
+module.exports = {
+  entry: './src/index.js',
+  mode: isDebug ? 'development' : 'production',
+  devtool: isDebug ? 'source-map' : false,
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: isDebug
+                  ? '[path][name]__[local]'
+                  : '[hash:base64:8]',
+              },
+              sourceMap: isDebug,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                require('cssnano')({
+                  zindex: false,
+                }),
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hot: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
